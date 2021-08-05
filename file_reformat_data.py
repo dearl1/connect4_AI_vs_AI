609555276920e1a93260f9caef21e94d60db5ef5
DEarl <59830842+dearl1@users.noreply.github.com>
2021-08-05 14:33:40 +0100

    Try to save last 3 moves of winning player & what other player should have played; I've over complicated it

--- file_reformat_data.py
+++ file_reformat_data.py
@@ -1,16 +1,14 @@
 
 import numpy as np
 
-def func_reformat_data(store_counter, store_choice, winner, store_n_times):
+def func_reformat_data(training_data, store_counter, store_choice, perspective, store_n_times):
 
     # each instance of test data is 42 element vector of 0, 1 or 2...
     # and next to that is a 7 element vector which is all 0 except a 1 where the choice was made
     sains = [ np.random.randn(y, x)
           for y, x in zip([42, 7], [1, 1]) ]
 
-    training_data = []
-
-    for i in range( store_n_times ):
+    for i in range( min([store_n_times, len(store_counter)]) ): # store_counter might not be as long as store_n_times
         training_data.append( [np.zeros(qwe.shape) for qwe in sains] )
 
         # fill the 42 element vector at the current i instance in training_data
@@ -18,7 +16,7 @@ def func_reformat_data(store_counter, store_choice, winner, store_n_times):
             training_data[i][0][counter_i] = store_counter[-1 - i][counter_i]
 
             # 0, 0.5, 1 for self, empty, other color
-            if winner == 1:
+            if perspective == 1:
                 if training_data[i][0][counter_i] == 1:
                     training_data[i][0][counter_i] = 0
                 elif training_data[i][0][counter_i] == 2:
@@ -26,7 +24,7 @@ def func_reformat_data(store_counter, store_choice, winner, store_n_times):
                 elif training_data[i][0][counter_i] == 0:
                     training_data[i][0][counter_i] = 0.5
                     
-            elif winner == 2:
+            elif perspective == 2:
                 if training_data[i][0][counter_i] == 1:
                     training_data[i][0][counter_i] = 1
                 elif training_data[i][0][counter_i] == 2:
--- make_training_data_from_AI_vs_AI.py
+++ make_training_data_from_AI_vs_AI.py
@@ -10,14 +10,17 @@ import file_AI_turn
 def func_make_training_data(biases_1, weights_1, biases_2, weights_2):
 
     winner = 0
-    play_again = True
+##    play_again = True
 
-    while play_again:
+    training_data = []
+
+    while len(training_data) == 0:
 
         counter = np.zeros((6, 7))
         ##print("\n counter ...\n", counter)
 
         whose_turn = 1
+        win_list = [0]
 
         store_counter_1 = []
         store_counter_2 = []
@@ -33,26 +36,6 @@ def func_make_training_data(biases_1, weights_1, biases_2, weights_2):
                 (counter, whose_turn, winner, game_status) = file_AI_turn.func_AI_turn(game_status, whose_turn, counter, biases_1, weights_1, winner, store_counter_1, store_counter_2, store_choice_1, store_choice_2)
             elif whose_turn == 2:
                 (counter, whose_turn, winner, game_status) = file_AI_turn.func_AI_turn(game_status, whose_turn, counter, biases_2, weights_2, winner, store_counter_1, store_counter_2, store_choice_1, store_choice_2)
-
-
-        import file_reformat_data
-        
-        if winner == 1:
-    ##        print("\n store_counter_1 ...\n", store_counter_1)
-    ##        print("\n store_choice_1 ...\n", store_choice_1)
-
-            training_data = file_reformat_data.func_reformat_data(store_counter_1, store_choice_1, winner)
-
-            
-        elif winner == 2:
-    ##        print("\n store_counter_2 ...\n", store_counter_2)
-    ##        print("\n store_choice_2 ...\n", store_choice_2)
-
-            training_data = file_reformat_data.func_reformat_data(store_counter_2, store_choice_2, winner)
-            
-
-        if game_status == "stopped": # if game_status == "draw" then play_again is not changed to False, so another game ensues
-            play_again = False
 
 
     ##print("\n training_data ...\n", training_data)
