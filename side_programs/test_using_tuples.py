2c44c354dc6a13c6e566e92091f74cd3e8f0c5c2
DEarl <59830842+dearl1@users.noreply.github.com>
2021-08-05 11:01:15 +0100

    Understand how to pass arguments to a function using a tuple

---
+++ side_programs/test_using_tuples.py
@@ -0,0 +1,11 @@
+
+
+qwe = ("small", 10, 10, 3.0)
+
+print("\n qwe ...\n", qwe)
+
+print("\n qwe[-1] ...\n", qwe[-1])
+
+print("\n qwe[0:-1] ...\n", qwe[0:-1])
+
+print("\n len(qwe) ...\n", len(qwe))
