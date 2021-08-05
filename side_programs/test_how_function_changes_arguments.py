09ed3af81b70a908d51161148fefee20ddc45994
DEarl <59830842+dearl1@users.noreply.github.com>
2021-08-05 19:52:44 +0100

    Look more into using arguments with functions

--- side_programs/test_how_function_changes_arguments.py
+++ side_programs/test_how_function_changes_arguments.py
@@ -1,6 +1,16 @@
 
+##def my_func(argum):
+##    argum = argum * 2
+##
+##argum = 1
+##print(argum)
+##
+##my_func(argum)
+##print(argum)
+
 def my_func(argum):
-    argum = argum * 2
+    argum = 2
+    return
 
 argum = 1
 print(argum)
