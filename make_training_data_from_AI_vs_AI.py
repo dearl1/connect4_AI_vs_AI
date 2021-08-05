702ac42b1cb6bf1f3b20a2c0f811c5c363f3ea12
DEarl <59830842+dearl1@users.noreply.github.com>
2021-08-05 17:05:27 +0100

    Start editing file

--- make_training_data_from_AI_vs_AI.py
+++ make_training_data_from_AI_vs_AI.py
@@ -12,7 +12,9 @@ def func_make_training_data(biases_1, weights_1, biases_2, weights_2):
     winner = 0
     play_again = True
 
-    while play_again:
+    training_data = []
+
+    while len(training_data) == 0:
 
         counter = np.zeros((6, 7))
         ##print("\n counter ...\n", counter)
@@ -35,24 +37,24 @@ def func_make_training_data(biases_1, weights_1, biases_2, weights_2):
                 (counter, whose_turn, winner, game_status) = file_AI_turn.func_AI_turn(game_status, whose_turn, counter, biases_2, weights_2, winner, store_counter_1, store_counter_2, store_choice_1, store_choice_2)
 
 
-        import file_reformat_data
-        
-        if winner == 1:
-    ##        print("\n store_counter_1 ...\n", store_counter_1)
-    ##        print("\n store_choice_1 ...\n", store_choice_1)
-
-            training_data = file_reformat_data.func_reformat_data(store_counter_1, store_choice_1, winner)
-
-            
-        elif winner == 2:
-    ##        print("\n store_counter_2 ...\n", store_counter_2)
-    ##        print("\n store_choice_2 ...\n", store_choice_2)
-
-            training_data = file_reformat_data.func_reformat_data(store_counter_2, store_choice_2, winner)
-            
-
-        if game_status == "stopped": # if game_status == "draw" then play_again is not changed to False, so another game ensues
-            play_again = False
+##        import file_reformat_data
+##        
+##        if winner == 1:
+##    ##        print("\n store_counter_1 ...\n", store_counter_1)
+##    ##        print("\n store_choice_1 ...\n", store_choice_1)
+##
+##            training_data = file_reformat_data.func_reformat_data(store_counter_1, store_choice_1, winner)
+##
+##            
+##        elif winner == 2:
+##    ##        print("\n store_counter_2 ...\n", store_counter_2)
+##    ##        print("\n store_choice_2 ...\n", store_choice_2)
+##
+##            training_data = file_reformat_data.func_reformat_data(store_counter_2, store_choice_2, winner)
+##            
+##
+##        if game_status == "stopped": # if game_status == "draw" then play_again is not changed to False, so another game ensues
+##            play_again = False
 
 
     ##print("\n training_data ...\n", training_data)
